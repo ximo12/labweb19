3ad31ce221b57dd02abf8bf614a9f542419ad80a
Quim Molina <quim@MacBook-Pro-de-Quim.local>
2018-10-15 22:15:20 +0200

    test

---
+++ src/main/java/tcm/quim/labweb/LabwebApplication.java
@@ -0,0 +1,12 @@
+package tcm.quim.labweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabwebApplication.class, args);
+    }
+}
---
+++ src/test/java/tcm/quim/labweb/LabwebApplicationTests.java
@@ -0,0 +1,16 @@
+package tcm.quim.labweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabwebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
