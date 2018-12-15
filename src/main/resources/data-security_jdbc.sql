4609bec37baca80ed855e82fb56867e837458927
Quim Molina <quim@MacBook-Pro-de-Quim.local>
2018-12-15 19:07:19 +0100

    no execute

--- src/main/resources/data-security_jdbc.sql
+++ src/main/resources/data-security_jdbc.sql
@@ -1,5 +1,4 @@
 
 INSERT INTO user_web (username, name, surname, mail, phone, password, date_create, date_edit, date_birth) VALUES ('quimmo', 'Quim', 'Molina', 'test@test.es', '123456789', '{noop}quim', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-
-//INSERT INTO authorities (username, role) VALUES ('quim', 'ROLE_ADMIN');
+INSERT INTO user_roles (username, role) VALUES ('quimmo', 'ROLE_USER');
--- src/main/resources/schema-security_jdbc.sql
+++ src/main/resources/schema-security_jdbc.sql
@@ -13,39 +13,13 @@ CREATE TABLE user_web
   date_birth TIMESTAMP,
 );
 
-DROP TABLE if EXISTS post_web;
-CREATE TABLE post_web (
-                        id bigint auto_increment PRIMARY KEY,
-                        title VARCHAR (100) ,
-                        text VARCHAR (255),
-                        date_create TIMESTAMP ,
-                        date_edit TIMESTAMP ,
-                        public BOOLEAN,
-                        owner VARCHAR (55),
-                        FOREIGN KEY (owner)
-                          REFERENCES user_web(mail)
-);
-
-DROP TABLE if EXISTS shared_post;
-CREATE TABLE shared_post (
-                           id_user bigint auto_increment PRIMARY KEY,
-                           title VARCHAR (100) ,
-                           text VARCHAR (255),
-                           date_create TIMESTAMP ,
-                           date_edit TIMESTAMP ,
-                           public BOOLEAN,
-                           owner VARCHAR (55),
-                           FOREIGN KEY (owner)
-                             REFERENCES user_web(mail)
-);
-
 
 DROP TABLE if EXISTS user_roles;
 CREATE TABLE user_roles (
-                          user_role_id int(11) NOT NULL AUTO_INCREMENT,
-                          username varchar(45) NOT NULL,
-                          role varchar(45) NOT NULL,
-                          PRIMARY KEY (user_role_id),
-                          UNIQUE KEY uni_username_role (role,username),
-                          CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES user_web (username));
+  user_role_id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  UNIQUE KEY uni_username_role (role,username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES user_web (username));
 
