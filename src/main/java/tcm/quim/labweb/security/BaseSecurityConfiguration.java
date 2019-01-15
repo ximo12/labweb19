0fe144c08ffc3648662b75db8fe63d468846029c
Quim Molina <quim@MacBook-Pro-de-Quim.local>
2019-01-15 18:51:22 +0100

    add routes to security

--- src/main/java/tcm/quim/labweb/security/BaseSecurityConfiguration.java
+++ src/main/java/tcm/quim/labweb/security/BaseSecurityConfiguration.java
@@ -16,10 +16,20 @@ public class BaseSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/getPosts").authenticated()
+                .antMatchers("/index").authenticated()
                 .antMatchers("/newPost").hasRole("USER")
+                .antMatchers("/editPost").hasRole("USER")
+                .antMatchers("/getPosts").hasRole("USER")
+                .antMatchers("/getMyPosts").hasRole("USER")
+                .antMatchers("/getPostById").hasRole("USER")
+                .antMatchers("/deletePost").hasRole("USER")
+                .antMatchers("/editUser").hasRole("USER")
+                .antMatchers("/getUsersThatImFriend").hasRole("USER")
+                .antMatchers("/getMyFriends").hasRole("USER")
+                .antMatchers("/addFriend").hasRole("USER")
+                .antMatchers("/deleteFriend").hasRole("USER")
 
-
+                
                 .anyRequest().authenticated()
                 .and()
             .formLogin() //to use forms (web)
